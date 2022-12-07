4336711ebe312fb309407e6fd55981c84286cdc6
Elkafhi-Youssef <elkafhiyoussef@gmail.com>
2022-12-07 13:47:09 +0100

    config tailwind

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -6,6 +6,22 @@ module.exports = {
   ],
   theme: {
     extend: {},
+    colors: {
+      'pri': '#0D1117',
+      'sec': '#1B78DE',
+      'pink': '#ff49db',
+    },
   },
   plugins: [require("daisyui")],
+  daisyui: {
+    themes: ["light", "dark", "cupcake", "bumblebee", "emerald", "corporate", "synthwave", "retro", "cyberpunk", "valentine", "halloween", "garden", "forest", "aqua", "lofi", "pastel", "fantasy", "wireframe", "black", "luxury", "dracula", "cmyk", "autumn", "business", "acid", "lemonade", "night", "coffee", "winter"],
+    styled: true,
+    themes: true,
+    base: true,
+    utils: true,
+    logs: true,
+    rtl: false,
+    prefix: "",
+    darkTheme: "dark",
+  },
 }
